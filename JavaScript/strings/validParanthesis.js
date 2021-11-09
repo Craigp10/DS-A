0842f4991e59cfe38cd6c3142d6379fa431e2889
Craig <CMPeoples3@gmail.com>
2021-11-09 09:37:25 -0800

    string manipulation... validParaenthesis implementation

---
+++ JavaScript/strings/validParanthesis.js
@@ -0,0 +1,28 @@
+function validParentheses(str) {
+  let stack = [];
+  let validObj = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+
+  if (str.length % 2 != 0) return false;
+
+  for (let i = 0; i < str.length; i++) {
+    if (validObj[str[i]]) {
+      stack.push(str[i]);
+    } else {
+      let last = stack.pop();
+      if (validObj[last] != str[i]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length ? false : true;
+}
+
+console.log(validParentheses("()"), true);
+console.log(validParentheses("(("), false);
+console.log(validParentheses("([{}])"), true);
+console.log(validParentheses("(([{()}]])"), false);
