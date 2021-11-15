c443e87f1f81a611db22699d835e396b8c322219
Craig <CMPeoples3@gmail.com>
2021-11-15 14:08:32 -0800

    String manipulation... improvements to longestSubSeqNoDups

--- JavaScript/strings/longestSubSeqNoDup.js
+++ JavaScript/strings/longestSubSeqNoDup.js
@@ -23,10 +23,10 @@ let longestSubSeqNoDup_brute = function (s) {
   return longestSeq.length;
 };
 
-console.log(longestSubSeqNoDup_brute("abcabc") == 3); //"abc"
-console.log(longestSubSeqNoDup_brute("abcbcad") == 4); //"bcad"
-console.log(longestSubSeqNoDup_brute("pwwkew") == 3); //"wke"
-console.log(longestSubSeqNoDup_brute("bbbbb") == 1); //"b"
+// console.log(longestSubSeqNoDup_brute("abcabc") == 3); //"abc"
+// console.log(longestSubSeqNoDup_brute("abcbcad") == 4); //"bcad"
+// console.log(longestSubSeqNoDup_brute("pwwkew") == 3); //"wke"
+// console.log(longestSubSeqNoDup_brute("bbbbb") == 1); //"b"
 
 const longestSubSeqNoDup_Sliding = function (s) {
   //Sliding window approach
@@ -58,7 +58,60 @@ const longestSubSeqNoDup_Sliding = function (s) {
   }
   return maxLength;
 };
-console.log(longestSubSeqNoDup_Sliding("abcabc") == 3);
-console.log(longestSubSeqNoDup_Sliding("pwwkew") == 3);
-console.log(longestSubSeqNoDup_Sliding("craigisbest") == 5);
-console.log(longestSubSeqNoDup_Sliding("bbbbb") == 1);
+// console.log(longestSubSeqNoDup_Sliding("abcabc") == 3);
+// console.log(longestSubSeqNoDup_Sliding("pwwkew") == 3);
+// console.log(longestSubSeqNoDup_Sliding("craigisbest") == 5);
+// console.log(longestSubSeqNoDup_Sliding("bbbbb") == 1);
+
+const longestSubSeqNoDup = function (s) {
+  //O(n) space complexity -- more space used
+  //Sliding window w/ Two indexes i (start), j (end).
+  //lastIndexDict -- Stores index of char occurences in our window. If we find dup we replace the dict key value w/ new j index.
+  if (!s || s.length == 1) {
+    return s.length;
+  }
+  let maxLength = 0;
+  let lastIndexDict = {}; //O(n) space
+  let i = 0;
+  for (let j = 0; j < s.length; j++) {
+    //O(n)
+    let current = s[j];
+    if (lastIndexDict[current] != undefined) {
+      //O(1)
+      i = Math.max(i, lastIndexDict[current] + 1); //Max between our current i (start) or the current value + 1, which would be our last i + 1 cause we just found a dup current
+    }
+
+    lastIndexDict[current] = j;
+
+    if (j + 1 - i > maxLength) maxLength = j + 1 - i;
+  }
+
+  return maxLength;
+};
+
+console.log(longestSubSeqNoDup("abcabc") == 3);
+console.log(longestSubSeqNoDup("pwwkew") == 3);
+console.log(longestSubSeqNoDup("craigisbest") == 5);
+console.log(longestSubSeqNoDup("bbbbb") == 1);
+console.log(longestSubSeqNoDup("abba") == 2);
+console.log(longestSubSeqNoDup("dvdf") == 3);
+
+const longestSubSeqNoDup_js = function (s) {
+  //Super fast method using the JS methods
+  if (!s) return 0;
+  var sub = "";
+  var max = 0;
+
+  for (var i = 0; i < s.length; i++) {
+    var l = s[i];
+    var match = sub.indexOf(l); //O(n)
+    if (match >= 0) {
+      max = Math.max(max, sub.length);
+      sub = sub.substring(match + 1) + l; //O(1)
+    } else {
+      sub += l;
+      max = Math.max(max, sub.length);
+    }
+  }
+  return max;
+};
