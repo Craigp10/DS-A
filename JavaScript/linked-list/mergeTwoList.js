f86a837f820b99ed8eaff03a3e1cbc419d30cac4
Craig <CMPeoples3@gmail.com>
2021-11-19 13:02:47 -0800

    mergeTwoList recursion implementation

--- JavaScript/linked-list/mergeTwoList.js
+++ JavaScript/linked-list/mergeTwoList.js
@@ -40,18 +40,35 @@ a1.forEach((val) => {
   prev1.next = new ListNode(val);
   prev1 = prev1.next;
 });
+
 prev1.next = null;
+
 a2.forEach((val) => {
   prev2.next = new ListNode(val);
   prev2 = prev2.next;
 });
+
 prev2.next = null;
 
 // console.log(l1.next, l2.next);
 
-let mergedLi = mergeTwoList(l1.next, l2.next);
+// let mergedLi = mergeTwoList(l1.next, l2.next);
 
-while (mergedLi) {
-  console.log(mergedLi);
-  mergedLi = mergedLi.next;
+// while (mergedLi) {
+//   console.log(mergedLi);
+//   mergedLi = mergedLi.next;
+// }
+
+function mergeTwoList_recurs(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoList_recurs(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoList_recurs(l1, l2.next);
+    return l2;
+  }
 }
+
+console.log(mergeTwoList_recurs(l1.next, l2.next));
