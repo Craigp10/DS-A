94e08c5388c0f11588ce532426fe95397c3a6c57
Craig <CMPeoples3@gmail.com>
2021-11-24 13:00:21 -0800

    oneAway bug fix... Didn't correctly consider missing chars case

--- JavaScript/strings/oneAway.js
+++ JavaScript/strings/oneAway.js
@@ -12,32 +12,33 @@ function isOneAwaySimple(str1, str2) {
   //add - the lengths will not match but a char will need to be inserted in a specified index
   //remove - the lengths will not match but a char will need to be removed from a specified index
   // I also need a way to count the edits along the way, if there are ever more than 1 return false
-
   let edits = 0;
   let i = 0; //str1 iterator
+  let j = 0; //str2 iteratorc
   //Brute Force, loop and for each of the edit options, we can loop the strs and compare
 
-  if (str1.length - str2.length > 1) return false;
+  if (Math.abs(str1.length - str2.length) > 1) return false;
   //return because there is garunteed more than 1 edit
   // else if (str1.length != str.length) edits++;
 
-  for (let j = 0; j < str2.length; j++) {
-    if (edits > 1) return false;
+  // for (let j = 0; j < str2.length; j++) {
+  while (edits < 2) {
+    if (j == str2.length) return true;
     if (str1[i] != str2[j]) {
       edits++;
       if (str1.length < str2.length) {
         //We must need to add a character
-        i++;
-        //increment i and skip over what we're assuming is a index we need to insert at
+        j++;
+        //increment j and skip over what we're assuming is a index we need to insert at. skip to next index on str2 and keep comparing
       } else if (str1.length > str2.length) {
         //We must need to remove a character
         i++;
-        //Assume the character we need to remove is the current index so lets jsut increment i and check the next index
+        //Assume the character we need to remove is the current index so lets just increment i and keep comparing
       } else {
-        //lengths are equal so we assume we just need to replace a character
+        //lengths are equal so we assume we just need to replace the character at the current index
         i++;
         j++;
-        //Increment both iterators and move forward by incrementing our edits
+        //Increment both iterators and keep comparing
       }
     } else {
       //index is good, move on to next
@@ -45,8 +46,16 @@ function isOneAwaySimple(str1, str2) {
       j++;
     }
   }
-  return true;
+
+  return false;
+
+  // if (str1.length > str2.length && edits == 1) return false;
+  // return true;
 }
 
 console.log(isOneAwaySimple("pale", "ple"), true);
 console.log(isOneAwaySimple("pale", "bake"), false);
+console.log(isOneAwaySimple("craig", "craigs"), true);
+console.log(isOneAwaySimple("andy isweird", "andy is weird"), true);
+console.log(isOneAwaySimple("andy is weird", "andi is wierd"), false);
+console.log(isOneAwaySimple("", "a"), true);
