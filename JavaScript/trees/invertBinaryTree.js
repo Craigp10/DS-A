198a32282bf3bf9cdabb6f5dacee04f24c55b940
Craig <CMPeoples3@gmail.com>
2022-02-18 17:27:20 -0800

    updating invert binary tree

--- JavaScript/trees/invertBinaryTree.js
+++ JavaScript/trees/invertBinaryTree.js
@@ -1,3 +1,5 @@
+const { BinaryTree } = require("../data-structures/binaryTree");
+
 /**
  * @param {TreeNode} root
  * @return {TreeNode}
@@ -8,23 +10,57 @@
  *[1,2,3,4,5,null,6] => [1,3,2,6,null,5,4];
  */
 
-var invertTree = function (root) {
+const invertTree_bfs = function (root) {
   if (!root) return null;
 
+  //create queue w/ root
   let q = [root];
+  //while q is not empty
   while (q.length) {
     //pop node and reassign left, right nodes
     let node = q.pop();
-    let left = node.left;
+    if (!node) continue;
+
+    let tmp = node.left;
     node.left = node.right;
-    node.right = left;
+    node.right = tmp;
+
     //If respective children nodes exist add to queue
-    if (node.left != null) {
-      q.push(node.left);
-    }
-    if (node.right != null) {
-      q.push(node.right);
-    }
+    // if (node.left != null) {
+    q.push(node.left);
+    // }
+    // if (node.right != null) {
+    q.push(node.right);
+    // }
   }
   return root;
 };
+let tree = new BinaryTree();
+tree.insert(1);
+tree.insert(2);
+tree.insert(3);
+tree.insert(4);
+tree.insert(5);
+tree.insert(null);
+tree.insert(6);
+// console.log(tree.root.right);
+// invertTree_bfs(tree.root);
+
+const invertTree_dfs = function (root) {
+  //if root is null return null
+  if (!root) return null;
+
+  let tmp = root.left;
+  root.left = root.right;
+  root.right = tmp;
+
+  //recursive tree with reverted 'parent' nodes
+  invertTree_dfs(root.right);
+  invertTree_dfs(root.left);
+
+  return root;
+};
+
+console.log(tree.printTree());
+invertTree_dfs(tree.root);
+// console.log(tree.printTree());
